fa236c314cd1d9a28c6df9b9db2d11719e363dcb
andi0b <andix25@gmail.com>
2022-12-02 21:44:53 +0100

    optimize day 2

--- Day02.fs
+++ Day02.fs
@@ -15,30 +15,28 @@ type GameResult =
     | Draw
 
 module Game =
-    // pick one shape, the it defeats the one to the left and forfeits against the one to the right
-    let orderedShapes = [ Rock; Paper; Scissor ]
-
-    let getDefeatForfeit shape =
+    let getShapeFromOffset offset shape =
+        // pick one shape, it defeats the one to the left and forfeits against the one to the right
+        let orderedShapes = [ Rock; Paper; Scissor ]
         let id = orderedShapes |> List.findIndex (fun x -> x = shape)
-        (orderedShapes.[(id + 3 - 1) % 3], orderedShapes.[(id + 1) % 3])
+        orderedShapes.[(id + 3 + offset) % 3]
 
-    let resultFromMoves theirMove myMove =
-        if myMove = theirMove then
-            Draw
-        else
-            let defeats, _ = getDefeatForfeit theirMove
-            if myMove = defeats then Lose else Win
+    let getWinningShape = getShapeFromOffset 1
+    let getLosingShape = getShapeFromOffset -1
 
-    let moveFromExpectedResult they expectedResult =
-        let defeats, forfeits = getDefeatForfeit they
+    let resultFromMoves theirShape myShape =
+        if myShape = theirShape then Draw
+        else if myShape = (theirShape |> getLosingShape) then Lose
+        else Win
 
+    let moveFromExpectedResult theirShape expectedResult =
         match expectedResult with
-        | Draw -> they
-        | Win -> forfeits
-        | Lose -> defeats
+        | Draw -> theirShape
+        | Win -> theirShape |> getWinningShape
+        | Lose -> theirShape |> getLosingShape
 
-    let score myMove result =
-        (match result with
+    let score myMove gameResult =
+        (match gameResult with
          | Win -> 6
          | Draw -> 3
          | Lose -> 0)
@@ -49,74 +47,68 @@ module Game =
 
 module Parser =
     let shapes = [| Rock; Paper; Scissor |]
-    let gameResults = [| Lose; Draw; Win |]
+    let parseShape1 chr = shapes.[int (chr - 'A')]
+    let parseShape2 chr = shapes.[int (chr - 'X')]
+    let parseGameResult chr = [| Lose; Draw; Win |].[int (chr - 'X')]
+    let splitLine map1 map2 (line: string) = (line.[0] |> map1, line.[2] |> map2)
 
-    let parseShape1 (x: char) = shapes.[int (x - 'A')]
-    let parseShape2 (x: char) = shapes.[int (x - 'X')]
-    let parseGameResult (x: char) = gameResults.[int (x - 'X')]
-
-    let splitLine (line: string) = (line.[0], line.[2])
-
-    let parsePart1 (lines: string array) =
-        lines |> Array.map (splitLine >> (fun (a, b) -> (parseShape1 a, parseShape2 b)))
-
-    let parsePart2 (lines: string array) =
-        lines
-        |> Array.map (splitLine >> (fun (a, b) -> (parseShape1 a, parseGameResult b)))
+    let parsePart1 = Array.map (splitLine parseShape1 parseShape2)
+    let parsePart2 = Array.map (splitLine parseShape1 parseGameResult)
 
 let part1 input =
     let moves = Parser.parsePart1 input
 
-    let moveScore (they, me) =
-        let result = Game.resultFromMoves they me
+    let scoreMove (theirShape, me) =
+        let result = Game.resultFromMoves theirShape me
         Game.score me result
 
-    moves |> Array.map moveScore |> Array.sum
+    moves |> Array.sumBy scoreMove
 
 let part2 input =
     let moves = Parser.parsePart2 input
 
-    let moveScore (they, expectedResult) =
-        let myMove = Game.moveFromExpectedResult they expectedResult
+    let scoreMove (theirShape, expectedResult) =
+        let myMove = Game.moveFromExpectedResult theirShape expectedResult
         Game.score myMove expectedResult
 
-    moves |> Array.map moveScore |> Array.sum
+    moves |> Array.sumBy scoreMove
 
 let run () =
     let input = File.ReadAllLines("inputs/day02.txt")
     $"Part 1: {part1 input} Part2: {part2 input}"
 
-module tests =
-    let example = [| "A Y"; "B X"; "C Z" |]
+let example = [| "A Y"; "B X"; "C Z" |]
 
+module ``Part 1 Tests`` =
     [<Fact>]
-    let ``parse example for part 1`` () =
+    let ``Parsing`` () =
         test <@ example |> Parser.parsePart1 = [| (Rock, Paper); (Paper, Rock); (Scissor, Scissor) |] @>
 
-
     [<Fact>]
-    let ``part 1 results should be Win,Lose,Draw`` () =
+    let ``round results should be Win,Lose,Draw`` () =
         test
-            <@ example
-               |> Parser.parsePart1
-               |> Array.map (fun (they, me) -> Game.resultFromMoves they me) = [| Win; Lose; Draw |] @>
+            <@
+                example
+                |> Parser.parsePart1
+                |> Array.map (fun (they, me) -> Game.resultFromMoves they me) = [| Win; Lose; Draw |]
+            @>
 
     [<Fact>]
-    let ``part 1 example output should be 15`` () = test <@ part1 example = 15 @>
+    let ``output should be 15`` () = test <@ part1 example = 15 @>
 
-
+module ``Part 2 Tests`` =
     [<Fact>]
-    let ``parse example for part 2`` () =
+    let ``Parsing`` () =
         test <@ example |> Parser.parsePart2 = [| (Rock, Draw); (Paper, Lose); (Scissor, Win) |] @>
 
     [<Fact>]
-    let ``part 2 moves should be Rock,Rock,Rock`` () =
+    let ``my moves should be Rock,Rock,Rock`` () =
         test
-            <@ example
-               |> Parser.parsePart2
-               |> Array.map (fun (they, expectedResult) -> Game.moveFromExpectedResult they expectedResult) = [| Rock
-                                                                                                                 Rock
-                                                                                                                 Rock |] @>
+            <@
+                example
+                |> Parser.parsePart2
+                |> Array.map (fun (they, result) -> Game.moveFromExpectedResult they result) = [| Rock; Rock; Rock |]
+            @>
 
     [<Fact>]
-    let ``part 2 example output should be 12`` () = test <@ part2 example = 12 @>
+    let ``output should be 12`` () = test <@ part2 example = 12 @>
